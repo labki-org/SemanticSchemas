0a89216a7d3c4f2d762733d48555330108ae2b76
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-01-28 18:01:21 -0800

    lint

--- tests/phpunit/integration/Hooks/CategoryPageHooksTest.php
+++ tests/phpunit/integration/Hooks/CategoryPageHooksTest.php
@@ -3,62 +3,62 @@
 namespace MediaWiki\Extension\SemanticSchemas\Tests\Integration\Hooks;
 
 use MediaWiki\Content\ContentHandler;
+use MediaWiki\Extension\SemanticSchemas\Hooks\CategoryPageHooks;
 use MediaWiki\Page\WikiPage;
 use MediaWiki\Revision\SlotRecord;
-use MediaWiki\Title\Title;
 use MediaWiki\Skin\Skin;
-
-use MediaWiki\Extension\SemanticSchemas\Hooks\CategoryPageHooks;
+use MediaWiki\Title\Title;
 use MediaWikiIntegrationTestCase;
 
 /**
  * @group Database
+ * @covers \MediaWiki\Extension\SemanticSchemas\Hooks\CategoryPageHooks
  */
 class CategoryPageHooksTest extends MediaWikiIntegrationTestCase {
-    private string $categoryName = "Category:TestCategory";
-    private WikiPage $page;
-    private Skin $skinMock;
-    private Title $title;
+	private string $categoryName = "Category:TestCategory";
+	private WikiPage $page;
+	private Skin $skinMock;
+	private Title $title;
 
-    protected function setUp(): void {
-        parent::setUp();
+	protected function setUp(): void {
+		parent::setUp();
 
-        $services = $this->getServiceContainer();
-        $this->title = Title::makeTitleSafe( NS_CATEGORY, $this->categoryName );
+		$services = $this->getServiceContainer();
+		$this->title = Title::makeTitleSafe( NS_CATEGORY, $this->categoryName );
 
-        $wikiPageFactory = $services->getWikiPageFactory();
-        $this->page = $wikiPageFactory->newFromTitle( $this->title );
+		$wikiPageFactory = $services->getWikiPageFactory();
+		$this->page = $wikiPageFactory->newFromTitle( $this->title );
 
-        $skinMock = $this->getMockBuilder( Skin::class )
-            ->disableOriginalConstructor()
-            ->getMock();
+		$skinMock = $this->getMockBuilder( Skin::class )
+			->disableOriginalConstructor()
+			->getMock();
 
-        $skinMock->method('getUser')
-            ->willReturn(static::getTestSysop()->getUser());
-        $skinMock->method("getTitle")
-            ->willReturn($this->title);
+		$skinMock->method( 'getUser' )
+			->willReturn( static::getTestSysop()->getUser() );
+		$skinMock->method( "getTitle" )
+			->willReturn( $this->title );
 
-        $this->skinMock = $skinMock;
-    }
+		$this->skinMock = $skinMock;
+	}
 
-    /**
-     * Rendering works normally even when the category is invalid
-     * @group Database
-     */
-    public function testRenderWhenCategoryInValid() {
-        $updater = $this->page->newPageUpdater( static::getTestSysop()->getUser() );
-        $updater->setContent(
-            SlotRecord::MAIN,
-            ContentHandler::makeContent(
-                '[[Has optional property::Property:A]] [[Has required property::Property:A]]',
-                $this->title
-            )
-        );
-        $updater->saveRevision("Made an invalid category schema");
-        $links = [];
-        ( new CategoryPageHooks )->onSkinTemplateNavigation($this->skinMock, $links);
+	/**
+	 * Rendering works normally even when the category is invalid
+	 * @group Database
+	 */
+	public function testRenderWhenCategoryInValid() {
+		$updater = $this->page->newPageUpdater( static::getTestSysop()->getUser() );
+		$updater->setContent(
+			SlotRecord::MAIN,
+			ContentHandler::makeContent(
+				'[[Has optional property::Property:A]] [[Has required property::Property:A]]',
+				$this->title
+			)
+		);
+		$updater->saveRevision( "Made an invalid category schema" );
+		$links = [];
+		( new CategoryPageHooks )->onSkinTemplateNavigation( $this->skinMock, $links );
 
-        $this->assertArrayHasKey('actions', $links);
-        $this->assertArrayHasKey('ss-generate-form', $links['actions']);
-    }
+		$this->assertArrayHasKey( 'actions', $links );
+		$this->assertArrayHasKey( 'ss-generate-form', $links['actions'] );
+	}
 }
