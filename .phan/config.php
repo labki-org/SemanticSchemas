3d3259f3751d20374e82ee450d484cc9c5160976
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-04-15 21:21:37 -0700

    add paths from composer deps to phan config

--- .phan/config.php
+++ .phan/config.php
@@ -6,25 +6,27 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
+$cfg['analyzed_file_extensions'] = [ 'php' ];
+$cfg['minimum_target_php_version'] = '8.1';
+
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
 		'src/',
 		'maintenance/',
-		'../../extensions/SemanticMediaWiki',
-		'../../extensions/PageForms',
+		__DIR__ . '/../core/includes/',
+		__DIR__ . '/../extensions/SemanticMediaWiki/includes/',
+		__DIR__ . '/../vendor/',
 	]
 );
 
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'],
 	[
-		'../../extensions/SemanticMediaWiki',
-		'../../extensions/PageForms',
-		'vendor/',
+		__DIR__ . '/../core/',
+		__DIR__ . '/../extensions/SemanticMediaWiki',
+		__DIR__ . '/../vendor/',
 	]
 );
-
-$cfg['analyzed_file_extensions'] = [ 'php' ];
 
 return $cfg;
