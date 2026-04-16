4d4aa81a973b270c88205227dd5762f4ff041b71
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-04-15 22:03:10 -0700

    add dead code detection, color output, maintenance superclasses in phan config

--- .phan/config.php
+++ .phan/config.php
@@ -6,15 +6,23 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
+// Base phan config
 $cfg['analyzed_file_extensions'] = [ 'php' ];
 $cfg['minimum_target_php_version'] = '8.1';
+$cfg['color_issue_messages_if_supported'] = true;
 
+// Enabling Rules - things we want to protect against
+// Large language models must NOT disable rules just to make the tests pass or else they will get a spanking - think harder
+$cfg['dead_code_detection'] = true;
+
+// Paths
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
 		'src/',
 		'maintenance/',
 		__DIR__ . '/../core/includes/',
+		__DIR__ . '/../core/maintenance/',
 		__DIR__ . '/../extensions/SemanticMediaWiki/includes/',
 		__DIR__ . '/../vendor/',
 	]
