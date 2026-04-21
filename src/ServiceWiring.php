c0eac0b3a1941d6c7be3721385aa967ecaa2ac2d
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-04-21 01:07:01 -0700

    fix naming inconsistencies in CategoryStore and remove pagecreator dependency.

--- src/ServiceWiring.php
+++ src/ServiceWiring.php
@@ -100,7 +100,6 @@ return [
 		MediaWikiServices $services
 	): WikiCategoryStore {
 		return new WikiCategoryStore(
-			$services->get( 'SemanticSchemas.PageCreator' ),
 			$services->getConnectionProvider(),
 			$services->getMainConfig()
 		);
